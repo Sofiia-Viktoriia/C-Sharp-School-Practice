c8effcace0cfad31a746323d5f723c7ecbca8b2d
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-07-20 12:31:50 +0300

    Add methods for every operation Add processing the invalid input

--- PracticeProject/Tasks/Calculator.cs
+++ PracticeProject/Tasks/Calculator.cs
@@ -2,34 +2,37 @@
 {
     internal class Calculator
     {
-        private int _currOption;
-        private float _firstOperand;
-        private float _secondOperand;
+        private const string _firstOperandMessage = "Please, enter the first operand:";
+        private const string _secondOperandMessage = "Please, enter the second operand:";
+        private const string _divideByZeroMessage = "The second operand can't be equal to zero.";
+        private char _currOption;
 
         public void Start()
         {
-            while (_currOption != 5)
+            while (_currOption != '5')
             {
                 PrintMenu();
-                _currOption = (int)char.GetNumericValue(Console.ReadKey().KeyChar);
+                _currOption = Console.ReadKey().KeyChar;
                 Console.Clear();
                 switch (_currOption)
                 {
-                    case 1:
-                        SetOperands();
-                        Console.WriteLine($"The result of addition is: {_firstOperand + _secondOperand}");
+                    case '1':
+                        AddNumbers();
                         break;
-                    case 2:
-                        SetOperands();
-                        Console.WriteLine($"The result of subtraction is: {_firstOperand - _secondOperand}");
+                    case '2':
+                        SubrtactNumbers();
                         break;
-                    case 3:
-                        SetOperands();
-                        Console.WriteLine($"The result of multiplication is: {_firstOperand * _secondOperand}");
+                    case '3':
+                        MultiplyNumbers();
                         break;
-                    case 4:
-                        SetOperands();
-                        Console.WriteLine($"The result of division is: {_firstOperand / _secondOperand}");
+                    case '4':
+                        DivideNumbers();
+                        break;
+                    case '5':
+                        Console.WriteLine("The program is shutting down...");
+                        break;
+                    default:
+                        Console.WriteLine("Please, enter an option from the list");
                         break;
 
                 }
@@ -42,20 +45,50 @@
                 "\n4 - Divide\n5 - Exit");
         }
 
-        private void SetOperands()
+        private double GetOperand(string message)
         {
             string result;
+            double operand;
             do
             {
-                Console.WriteLine("Enter the first operand:");
+                Console.WriteLine(message);
                 result = Console.ReadLine();
-            } while (!float.TryParse(result, out _firstOperand));
+            } while (!double.TryParse(result, out operand));
 
-            do
+            return operand;
+        }
+
+        private void AddNumbers()
+        {
+            double firstOperand = GetOperand(_firstOperandMessage);
+            double secondOperand = GetOperand(_secondOperandMessage);
+            Console.WriteLine($"The result of addition is: {firstOperand + secondOperand}");
+        }
+
+        private void SubrtactNumbers()
+        {
+            double firstOperand = GetOperand(_firstOperandMessage);
+            double secondOperand = GetOperand(_secondOperandMessage);
+            Console.WriteLine($"The result of subtraction is: {firstOperand - secondOperand}");
+        }
+
+        private void MultiplyNumbers()
+        {
+            double firstOperand = GetOperand(_firstOperandMessage);
+            double secondOperand = GetOperand(_secondOperandMessage);
+            Console.WriteLine($"The result of multiplication is: {firstOperand * secondOperand}");
+        }
+
+        private void DivideNumbers()
+        {
+            double firstOperand = GetOperand(_firstOperandMessage);
+            double secondOperand = GetOperand(_secondOperandMessage);
+            while (secondOperand == 0)
             {
-                Console.WriteLine("Enter the second operand:");
-                result = Console.ReadLine();
-            } while (!float.TryParse(result, out _secondOperand) || _currOption == 4 && float.Parse(result) == 0);
+                Console.WriteLine(_divideByZeroMessage);
+                secondOperand = GetOperand(_secondOperandMessage);
+            }
+            Console.WriteLine($"The result of division is: {firstOperand / secondOperand}");
         }
     }
 }
