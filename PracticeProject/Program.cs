58f7892d168eda14d97a74d3f752bb95e8479730
Sofiia-Viktoriia Kovalova <83633927+Sofiia-Viktoriia@users.noreply.github.com>
2023-07-20 14:21:24 +0300

    Revert "Add the solution for data types and console input/output task"

--- PracticeProject/Program.cs
+++ PracticeProject/Program.cs
@@ -1,15 +1,7 @@
-﻿using PracticeProject.Tasks;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        var userData = new UserDataBuilder()
-            .SetName()
-            .SetEmail()
-            .SetAge()
-            .SetPassword()
-            .Build();
-        userData.PrintResult();
+        Console.WriteLine("Hello, World!");
     }
 }
