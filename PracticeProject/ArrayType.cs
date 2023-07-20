a78b2d89b83c7911cd1371d28ff8e883be67968c
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-07-20 15:43:12 +0300

    Add the solution for methods task

---
+++ PracticeProject/ArrayType.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject
+{
+    internal class ArrayType
+    {
+        public static int[] CreateArrayWithConsole(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                string result;
+                do
+                {
+                    result = Console.ReadLine();
+                } while (!int.TryParse(result, out array[i]));
+            }
+            return array;
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            foreach (int item in array)
+            {
+                Console.Write(item + "\t");
+            }
+        }
+
+        public static void SortDesc(ref int[] array)
+        {
+            for (int pass = 0; pass <= array.Length - 2; pass++)
+            {
+                for (int loop = 0; loop <= array.Length - 2; loop++)
+                {
+                    if (array[loop] < array[loop + 1])
+                    {
+                        int temp = array[loop + 1];
+                        array[loop + 1] = array[loop];
+                        array[loop] = temp;
+                    }
+                }
+            }
+        }
+
+        public static bool Equal(int[] firstArray, int[] secondArray)
+        {
+            if (firstArray == null || secondArray == null)
+                return false;
+
+            if (firstArray.Length != secondArray.Length)
+                return false;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] != secondArray[i]) return false;
+            }
+            return true;
+        }
+    }
+}
--- PracticeProject/Program.cs
+++ PracticeProject/Program.cs
@@ -1,7 +1,36 @@
-﻿internal class Program
+﻿using PracticeProject;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int arraySize = 5;
+        Console.WriteLine("Please, enter numbers for the first array:");
+        int[] firstArray = ArrayType.CreateArrayWithConsole(arraySize);
+        
+        Console.WriteLine("Please, enter numbers for the second array:");
+        int[] secondArray = ArrayType.CreateArrayWithConsole(arraySize);
+
+        Console.Clear();
+        Console.WriteLine("First array before sort:");
+        ArrayType.PrintArray(firstArray);
+        Console.WriteLine("\nSecond array before sort:");
+        ArrayType.PrintArray(secondArray);
+
+        ArrayType.SortDesc(ref firstArray);
+        ArrayType.SortDesc(ref secondArray);
+        Console.WriteLine("\nFirst array after sort:");
+        ArrayType.PrintArray(firstArray);
+        Console.WriteLine("\nSecond array after sort:");
+        ArrayType.PrintArray(secondArray);
+
+        if (ArrayType.Equal(firstArray, secondArray))
+        {
+            Console.WriteLine("\n\nThe given arrays are equal");
+        }
+        else
+        {
+            Console.WriteLine("\n\nThe given arrays are not equal");
+        }
     }
 }
