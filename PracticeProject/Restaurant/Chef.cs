f13879546de9a17e737bd15c01e6a68e286cf866
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-07-24 13:42:30 +0300

    Add a solution for oop principles task

---
+++ PracticeProject/Restaurant/Chef.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject.Restaurant
+{
+    internal class Chef
+    {
+        private void MakeSpecialDish()
+        {
+            Console.WriteLine("Make a specialty soup");
+        }
+
+        public virtual void MakeSteak()
+        {
+            Console.WriteLine("Make a well-cooked steak");
+        }
+    }
+}
---
+++ PracticeProject/University/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject.University
+{
+    internal class Teacher : Person
+    {
+        private string[] _subjects;
+        private string[] _groups;
+    }
+}
