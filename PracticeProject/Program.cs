94113ebde6d35b28110a9743425144b4c0f4d93c
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-07-24 14:51:34 +0300

    Add a solution for objects, classes and structures task

--- PracticeProject/Program.cs
+++ PracticeProject/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using PracticeProject;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        User user = new User("Kate", 142, 23);
+        User.ChangeAge(user, 15);
+        user.PrintInformation();
     }
 }
---
+++ PracticeProject/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject
+{
+    internal class User
+    {
+        public string _name { get; private set; }
+        public int _id { get; private set; }
+        public int _age { get; private set; }
+
+        public User(string name, int id, int age)
+        {
+            _name = name;
+            _id = id;
+            _age = age;
+        }
+
+        public static void ChangeAge(User user, int age)
+        {
+            user._age = age;
+        }
+
+        public void PrintInformation()
+        {
+            Console.WriteLine($"Name: {_name}\nId: {_id}\nAge: {_age}");
+        }
+    }
+}
