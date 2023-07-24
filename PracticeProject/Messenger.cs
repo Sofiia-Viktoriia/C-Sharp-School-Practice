cc25e40eced93bc8c80d38fd9a39431bb2369ed8
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-07-24 16:11:37 +0300

    Add a solution for constructors, destructors, and deconstructors task

---
+++ PracticeProject/Messenger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject
+{
+    internal class Messenger
+    {
+        public static int State { get; set; }
+        public string Message { get; set; }
+        public int MessageNumber { get; set; }
+
+        static Messenger()
+        {
+            State = 2;
+        }
+
+        public Messenger(string message, int messageNumber)
+        {
+            this.Message = message;
+            this.MessageNumber = messageNumber;
+        }
+
+        public void Deconstruct(out string message, out int messageNumber)
+        {
+            message = Message;
+            messageNumber = MessageNumber;
+        }
+    }
+}
--- PracticeProject/Program.cs
+++ PracticeProject/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using PracticeProject;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Messenger messenger = new Messenger("Message 1", 1);
+        (string message, int number) = messenger;
+        Console.WriteLine($"Message: {message}\nMessage number: {number}\nState: {Messenger.State}");
     }
 }
