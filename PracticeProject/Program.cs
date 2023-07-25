16890a035e3df99c13a9d47e9197b06d0ffadd58
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-07-25 17:56:15 +0300

    Add a solution for inheritance task

--- PracticeProject/Program.cs
+++ PracticeProject/Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using PracticeProject;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Shape shape = new Shape(12, 20);
+        shape.GetArea();
+
+        Rectangle rectangle = new Rectangle(23, 44);
+        rectangle.GetArea();
+
+        object obj1 = new Shape(32, 667);
+        object obj2 = new Rectangle(43, 11);
+
+        Shape shape1 = obj1 as Shape;
+        shape1?.GetArea();
+
+        if (obj2 is Rectangle r)
+        {
+            r.GetArea();
+        }
+        else
+        {
+            Console.WriteLine("Error while converting");
+        }
     }
 }
---
+++ PracticeProject/Rectangle.cs
@@ -0,0 +1,15 @@
+﻿namespace PracticeProject
+{
+    internal class Rectangle : Shape
+    {
+        public Rectangle(int width, int height) : base(width, height)
+        {
+            Console.WriteLine("Rectangle is created");
+        }
+
+        public void GetArea()
+        {
+            Console.WriteLine($"The area of the rectangle equals {Width * Height}");
+        }
+    }
+}
