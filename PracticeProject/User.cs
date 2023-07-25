54cddfdb47707d00f0957991d460079f405e91c0
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-07-25 13:11:49 +0300

    Refactor fields and using

--- PracticeProject/User.cs
+++ PracticeProject/User.cs
@@ -1,32 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PracticeProject
+﻿namespace PracticeProject
 {
     internal class User
     {
-        public string _name { get; private set; }
-        public int _id { get; private set; }
-        public int _age { get; private set; }
+        public readonly string Name;
+        public readonly int Id;
+        public int Age { get; private set; }
 
         public User(string name, int id, int age)
         {
-            _name = name;
-            _id = id;
-            _age = age;
+            Name = name;
+            Id = id;
+            Age = age;
         }
 
         public static void ChangeAge(User user, int age)
         {
-            user._age = age;
+            user.Age = age;
         }
 
         public void PrintInformation()
         {
-            Console.WriteLine($"Name: {_name}\nId: {_id}\nAge: {_age}");
+            Console.WriteLine($"Name: {Name}\nId: {Id}\nAge: {Age}");
         }
     }
 }
