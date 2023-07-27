ced0353083a49cbbb1a905c9601031ce60231fc1
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-07-27 10:17:31 +0300

    Add a solution for generics task

---
+++ PracticeProject/User.cs
@@ -0,0 +1,11 @@
+﻿namespace PracticeProject
+{
+    internal class User
+    {
+        private int _userId;
+        private double _balance;
+
+        public int UserId { get; set; }
+        public double Balance { get; set; }
+    }
+}
