ec8dff1db1faf34cd9c9d1bd88043702fa58233d
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-07-27 10:27:24 +0300

    Add properties for User class

--- PracticeProject/User.cs
+++ PracticeProject/User.cs
@@ -5,7 +5,25 @@
         private int _userId;
         private double _balance;
 
-        public int UserId { get; set; }
-        public double Balance { get; set; }
+        public int UserId { 
+            get
+            {
+                return _userId;
+            }
+            set
+            {
+                _userId = value;
+            }
+        }
+        public double Balance {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
     }
 }
