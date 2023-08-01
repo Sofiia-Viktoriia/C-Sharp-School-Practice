525d21d22e0f78071566d2e4f672fd542e9d2256
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-08-01 15:11:29 +0300

    Refactor the code Change regex for searching error records Move files

--- PracticeProject/Program.cs
+++ PracticeProject/Program.cs
@@ -5,7 +5,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string path = "..\\..\\..\\yupdate.log";
+        string path = "yupdate.log";
         int ratio = FindErrors(path);
         if (ratio > 0)
         {
@@ -15,38 +15,19 @@ internal class Program
 
     public static int FindErrors(string path)
     {
-        string line = string.Empty;
-        int allEntries = 0;
-        int errorEntries = 0;
-        Regex enrtyRegex = new Regex(@".*\d{2}:\d{2}:\d{2} [DIE]:.*$");
-        Regex errorRegex = new Regex(@".*\d{2}:\d{2}:\d{2} [E]:.*$");
-        StreamReader? streamReader = null;
-        FileStream? fileStream = null;
-        StreamWriter? streamWriter = null;
+        string[] allRecords;
+        string[] errorRecords;
         try
         {
-            streamReader = File.OpenText(path);
-            fileStream = File.OpenWrite("..\\..\\..\\error.log");
-            streamWriter = new StreamWriter(fileStream);
-
-            while ((line = streamReader.ReadLine()) != null)
-            {
-                if (enrtyRegex.IsMatch(line))
-                {
-                    allEntries++;
-                }
-                if (errorRegex.IsMatch(line))
-                {
-                    errorEntries++;
-                    streamWriter.WriteLine(line);
-                    CheckIfCritical(line);
-                }
-            }
-            return allEntries / errorEntries;
+            allRecords = ReadFile(path);
+            errorRecords = FindErrorRecords(allRecords);
+            WriteErrorRecordsToFile(errorRecords);
+            CheckIfErrorRecordsAreCritical(errorRecords);
+            return allRecords.Length / errorRecords.Length;
         }
         catch (FileNotFoundException ex)
         {
-            Console.WriteLine($"Error: {ex.Message}");
+            Console.WriteLine($"Error: {ex}");
             return -1;
         }
         catch (DivideByZeroException)
@@ -56,37 +37,67 @@ internal class Program
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error occurred: {ex.Message}");
+            Console.WriteLine($"Error occurred: {ex}");
             return -1;
-        }
-        finally
-        {
-            try
-            {
-                streamReader?.Close();
-                streamWriter?.Close();
-                fileStream?.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error occurred while closing the file: {ex.Message}");
-            }
         }
     }
 
-    public static void CheckIfCritical(string line)
+    public static void WriteErrorRecordsToFile(string[] records)
     {
-        try
+        File.WriteAllLines("error.log", records);
+    }
+
+    public static string[] FindErrorRecords(string[] records)
+    {
+        Regex regex = new Regex(@"(error|Error|ERROR)");
+        return records.Where(record => regex.IsMatch(record)).ToArray();
+    }
+
+    public static string[] ReadFile(string path)
+    {
+        List<string> records = new List<string>();
+        Regex enrtyRegex = new Regex(@"\d{2}:\d{2}:\d{2} .:");
+        using (StreamReader streamReader = File.OpenText(path))
         {
-            if (line.Contains("CRITICAL ERROR"))
+            string currentRecord = "";
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
             {
-                Console.WriteLine(line);
-                throw new CriticalErrorException("Critical error found in the file.");
+                if (enrtyRegex.IsMatch(line))
+                {
+                    if (!string.IsNullOrEmpty(currentRecord))
+                    {
+                        records.Add(currentRecord);
+                        currentRecord = "";
+                    }
+                }
+                currentRecord += line;
+            }
+
+            if (!string.IsNullOrEmpty(currentRecord))
+            {
+                records.Add(currentRecord);
             }
         }
-        catch (CriticalErrorException ex)
+        return records.ToArray();
+    }
+
+    public static void CheckIfErrorRecordsAreCritical(string[] records)
+    {
+        foreach (string record in records)
         {
-            Console.WriteLine($"Critical Error: {ex.Message}");
+            try
+            {
+                if (record.Contains("CRITICAL ERROR"))
+                {
+                    Console.WriteLine(record);
+                    throw new CriticalErrorException("Critical error found in the file.");
+                }
+            }
+            catch (CriticalErrorException ex)
+            {
+                Console.WriteLine($"Critical Error: {ex}");
+            }
         }
     }
 }
