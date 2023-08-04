0d0139692808d29ec0c0d2ff17288098f87e01c4
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-08-04 16:25:47 +0300

    Add a solution for Page Object Model task

---
+++ PracticeProject/LoginRegistrationPage.cs
@@ -0,0 +1,55 @@
+﻿namespace PageObjectProject
+{
+    public class LoginRegistrationPage
+    {
+        IWebDriver _webDriver;
+        private IWebElement _formTitle(string title) => _webDriver.FindElement(By.XPath($"//h2[text()='{title}']"));
+        private IWebElement _loginFormUsernameOrEmailInput => _webDriver.FindElement(By.XPath("//input[@id = 'username']"));
+        private IWebElement _loginFormUsernameOrEmailLabel => _webDriver.FindElement(By.XPath("//label[@for='username']"));
+        private IWebElement _loginFormPasswordInput => _webDriver.FindElement(By.XPath("//input[@id = 'password']"));
+        private IWebElement _loginFormPasswordLabel => _webDriver.FindElement(By.XPath("//label[@for='password']"));
+        private IWebElement _loginButton => _webDriver.FindElement(By.XPath("//input[@name = 'login']"));
+        private IWebElement _rememberMeCheckbox => _webDriver.FindElement(By.XPath("//input[@id = 'rememberme']"));
+        private IWebElement _rememberMeLabel => _webDriver.FindElement(By.XPath("//label[@for='rememberme']"));
+        private IWebElement _lostPasswordLink => _webDriver.FindElement(By.XPath("//p[contains(concat(' ', @class, ' '), ' lost_password ')]/a"));
+        private IWebElement _registerFormEmailInput => _webDriver.FindElement(By.XPath("//input[@id = 'reg_email']"));
+        private IWebElement _registerFormEmailLabel => _webDriver.FindElement(By.XPath("//label[@for='reg_email']"));
+        private IWebElement _registerFormPasswordInput => _webDriver.FindElement(By.XPath("//input[@id = 'reg_password']"));
+        private IWebElement _registerFormPasswordLabel => _webDriver.FindElement(By.XPath("//label[@for='reg_password']"));
+        private IWebElement _registerButton => _webDriver.FindElement(By.XPath("//input[@name = 'register']"));
+        private IWebElement _errorMessage => _webDriver.FindElement(By.XPath("//li[./strong[text()='Error:']]"));
+
+        public LoginRegistrationPage(IWebDriver webDriver)
+        {
+            this._webDriver = webDriver;
+        }
+
+        public LoginRegistrationPage EnterEmailAndPasswordAndClickLoginButton(string email, string password)
+        {
+            _loginFormUsernameOrEmailInput.SendKeys(email);
+            _loginFormPasswordInput.SendKeys(password);
+            _loginButton.Click();
+            return this;
+        }
+
+        public string GetLostPasswordLinkValue()
+        {
+            return _lostPasswordLink.Text;
+        }
+
+        public string GetRememberMeLabelValue()
+        {
+            return _rememberMeLabel.Text;
+        }
+
+        public string GetRegisterButtonValue()
+        {
+            return _registerButton.GetAttribute("value");
+        }
+
+        public bool IsErrorMessageDisplayed()
+        {
+            return _errorMessage.Displayed;
+        }
+    }
+}
---
+++ PracticeProject/LoginRegistrationPageFactory.cs
@@ -0,0 +1,59 @@
+﻿using SeleniumExtras.PageObjects;
+
+namespace PageObjectProject
+{
+    public class LoginRegistrationPageFactory
+    {
+        private IWebDriver _webDriver;
+
+        public LoginRegistrationPageFactory(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+            PageFactory.InitElements(_webDriver, this);
+        }
+
+        public IWebElement FormTitle(string title) => _webDriver.FindElement(By.XPath($"//h2[text()='{title}']"));
+
+        [FindsBy(How = How.XPath, Using = "//input[@id = 'username']")]
+        public IWebElement LoginFormUsernameOrEmailInput { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//label[@for='username']")]
+        public IWebElement LoginFormUsernameOrEmailLabel { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@id = 'password']")]
+        public IWebElement LoginFormPasswordInput { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//label[@for='password']")]
+        public IWebElement LoginFormPasswordLabel { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@name = 'login']")]
+        public IWebElement LoginButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@id = 'rememberme']")]
+        public IWebElement RememberMeCheckbox { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//label[@for='rememberme']")]
+        public IWebElement RememberMeLabel { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//p[contains(concat(' ', @class, ' '), ' lost_password ')]/a")]
+        public IWebElement LostPasswordLink { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@id = 'reg_email']")]
+        public IWebElement RegisterFormEmailInput { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//label[@for='reg_email']")]
+        public IWebElement RegisterFormEmailLabel { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@id = 'reg_password']")]
+        public IWebElement RegisterFormPasswordInput { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//label[@for='reg_password']")]
+        public IWebElement RegisterFormPasswordLabel { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@name = 'register']")]
+        public IWebElement RegisterButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//li[./strong[text()='Error:']]")]
+        public IWebElement ErrorMessage { get; set; }
+    }
+}
