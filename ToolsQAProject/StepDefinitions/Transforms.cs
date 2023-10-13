3872dfc328ab7d63a2ed4ac82ef75188c0a08838
Sofiia-Viktoriia <sofiia.viktoriia.kovalova@gmail.com>
2023-10-13 15:58:59 +0300

    Refactor methods in the Transforms class

--- ToolsQAProject/StepDefinitions/Transforms.cs
+++ ToolsQAProject/StepDefinitions/Transforms.cs
@@ -9,39 +9,25 @@ namespace ToolsQAProject.StepDefinitions
     public class Transforms
     {
         [StepArgumentTransformation]
-        public string[] StringValuesTransform(Table table)
+        public string[] TableToStringArray(Table table)
         {
             return table.Rows.Select(r => r[0]).ToArray();
         }
 
-        [StepArgumentTransformation(@"user selects squares")]
-        public int[] SquaresNumbersTransform(Table table)
+        [StepArgumentTransformation]
+        public int[] TableToIntArray(Table table)
         {
             return table.Rows.Select(r => int.Parse(r[0])).ToArray();
         }
 
         [StepArgumentTransformation]
-        public StudentRegistrationForm StudentRegistrationFormTransform(Table table)
+        public StudentRegistrationForm TableToStudentRegistrationForm(Table table)
         {
-            return new StudentRegistrationForm
-            {
-                FirstName = table.Rows[0][nameof(StudentRegistrationForm.FirstName)],
-                LastName = table.Rows[0][nameof(StudentRegistrationForm.LastName)],
-                Email = table.Rows[0][nameof(StudentRegistrationForm.Email)],
-                Gender = table.Rows[0][nameof(StudentRegistrationForm.Gender)],
-                MobilePhone = table.Rows[0][nameof(StudentRegistrationForm.MobilePhone)],
-                DateOfBirth = DateTime.ParseExact(table.Rows[0][nameof(StudentRegistrationForm.DateOfBirth)], 
-                    "dd MMMM yyyy", CultureInfo.InvariantCulture),
-                Subjects = table.Rows[0][nameof(StudentRegistrationForm.Subjects)].Split("; ").ToList(),
-                Hobbies = table.Rows[0][nameof(StudentRegistrationForm.Hobbies)].Split("; ").ToList(),
-                CurrentAddress = table.Rows[0][nameof(StudentRegistrationForm.CurrentAddress)],
-                State = table.Rows[0][nameof(StudentRegistrationForm.State)],
-                City = table.Rows[0][nameof(StudentRegistrationForm.City)]
-            };
+            return table.CreateInstance<StudentRegistrationForm>();
         }
 
         [StepArgumentTransformation]
-        public UserForm UserFormTransform(Table table)
+        public UserForm TableToUserForm(Table table)
         {
             return table.CreateInstance<UserForm>();
         }
